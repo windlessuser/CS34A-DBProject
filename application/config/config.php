04a7527563212c0d3b22af227df79ae632e6c31c
windlessuser <windlessuser@gmail.com>
2011-03-29 20:30:46 +0000

    prelim changes; set up for real work

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/esub3/';
+$config['base_url']	= 'http://localhost/cs34a-dbproject/';
 
 /*
 |--------------------------------------------------------------------------
