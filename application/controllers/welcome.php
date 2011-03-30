aa1bfd8dc29463b034fa18724783a66b949ca456
marc Byfield <windlessuser@gmail.com>
2011-03-30 08:28:02 -0500

    Added welcome page functions

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -9,7 +9,67 @@ class Welcome extends CI_Controller {
 
 	function index()
 	{
-		$this->load->view('welcome_message');
+		
+		$data['main_content'] = 'login_form';
+		$this->load->view('includes/template', $data);		
+	}
+	
+	function validate_credentials()
+	{		
+		$this->load->model('membership_model');
+		$query = $this->membership_model->validate();
+		
+		if($query) // if the user's credentials validated...
+		{
+			$data = array(
+				'username' => $this->input->post('username'),
+				'is_logged_in' => true
+			);
+			$this->session->set_userdata($data);
+			redirect('site/members_area');
+		}
+		else // incorrect username or password
+		{
+			$this->index();
+		}
+	}	
+	
+	function signup()
+	{
+		$data['main_content'] = 'signup_form';
+		$this->load->view('includes/template', $data);
+	}
+	
+	function create_member()
+	{
+		$this->load->library('form_validation');
+		
+		if($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('signup_form');
+		}
+		
+		else
+		{			
+			$this->load->model('membership_model');
+			
+			if($query = $this->membership_model->create_member())
+			{
+				$data['main_content'] = 'signup_successful';
+				$this->load->view('includes/template', $data);
+			}
+			else
+			{
+				$this->load->view('signup_form');			
+			}
+		}
+		
+	}
+	
+	function logout()
+	{
+		$this->session->sess_destroy();
+		$this->index();
 	}
 }
 
