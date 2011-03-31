13bcb0e5e39b58ce4ee041e4f520e11933583c8d
labak55 <adrian.alex.williams@gmail.com>
2011-03-31 19:44:42 +0000

    the starting tables

---
+++ sql/tables.sql
@@ -0,0 +1,36 @@
+create table Itype
+(      Item_type   varchar(10)  not null,
+       primary key(Item_type)
+);
+
+create table Brands
+(      Brand       varchar(10)  not null,
+       primary key(Brand)
+);
+
+create table Brand
+(      Item_no    varchar(10)   not null,
+       Item_type  varchar(10)   not null,
+       Brand      varchar(10)   not null,
+       quant      int(4)        not null,
+       primary key(item_no),
+       foreign key(Item_type) references Itype(item_type),
+       foreign key(Brand) references Brands(Brand)
+)Engine = INNODB;
+
+
+
+create table User
+(      fname     varchar(20)    not null,
+       lname     varchar(20)    not null,
+       uname     varchar(20)    not null,
+       pass      char(40)       not null,
+       bool      numeric(1)     not null,
+       primary key(uname),
+       check(bool = '0' or bool = '1')
+)Engine = INNODB;
+
+
+create table Store
+(      
+
