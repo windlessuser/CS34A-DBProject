d94d5da7bd0bbce6d506f3d529949b72413edba4
LadiiCaribe <char.caye@gmail.com>
2011-04-05 15:10:10 -0700

    Edited sql/database.sql via GitHub

--- sql/database.sql
+++ sql/database.sql
@@ -1,12 +1,12 @@
 create table IF NOT EXISTS   UsersInfo (	
                 useid                int(5) auto_increment not null,       
-                username 		varchar(15)	not null,
-                password		char(40) 	not null,
-                first_name 	varchar(10)	not null,
-		last_name		varchar(10)	not null,
-		email			varchar(30)	not null,
-		is_admin		boolean		not null,
-		joined		timestamp	not null,
+                username 		varchar(15)	   not null,
+                password		char(40) 	   not null,
+                first_name 	varchar(10)	   not null,
+		last_name		varchar(10)	    not null,
+		email			varchar(30)	    not null,
+		is_admin		boolean	     not null,
+		joined		timestamp	     not null,
 		primary key(useid)
 );
 
